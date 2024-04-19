1656dec2bbc1c4aa1a1d61946fb02fb4bbc4a6d5
sengchhunyeang <chhunyeangseng@gmail.com>
2024-04-19 18:50:28 +0700

    done with exception

--- src/main/java/org/kshrd/repository/ExpenseRepository.java
+++ src/main/java/org/kshrd/repository/ExpenseRepository.java
@@ -1,6 +1,9 @@
 package org.kshrd.repository;
 
 import org.apache.ibatis.annotations.*;
+import org.kshrd.model.dto.request.CategoryRequest;
+import org.kshrd.model.dto.request.ExpenseRequest;
+import org.kshrd.model.entity.Category;
 import org.kshrd.model.entity.Expense;
 import java.util.List;
 
@@ -20,20 +23,38 @@ public interface ExpenseRepository {
             one = @One(select = "org.kshrd.repository.AppUserRepository.getCategoryById")
             )
     })
-    List<Expense> getAllExpense(@Param("offset") Integer offset, @Param("limit") Integer limit);
+    List<Expense> getAllExpense(@Param("offset") Integer offset, @Param("limit") Integer limit, @Param("currentUser") String currentUser);
+
     @Select("""
         SELECT * FROM expenses
         WHERE expense_id = #{expenseId}
        """)
     @ResultMap("expenseMapping")
-    List<Expense> getExpenseById(Integer expenseId);
+    Expense getExpenseById(@Param("expenseId") Integer id, @Param("currentUser") String currentUser);
 
     @Select("""
         DELETE FROM expenses
         WHERE expense_id = #{id}
         """)
     @ResultMap("expenseMapping")
-    Expense deleteExpenseById(Integer id);
+    void deleteCategory (@Param("id") Integer id, @Param("currentUser")String currentUser);
+
+
+    @Select("""
+            INSERT INTO expenses(amount,description,date,category_id,user_id) VALUES(
+            #{expense.amount},
+            #{expense.amount},#{expense.date},#{expense.categoryId}, (SELECT user_id FROM users WHERE email = #{currentUser})) RETURNING *
+        """)
+    @ResultMap("expenseMapping")
+    Expense insertExpense(@Param("expense")ExpenseRequest expenseRequest,@Param("currentUser")  String user);
+
+    @Select("""
+            UPDATE expenses SET amount = #{expense.amount}, description = #{expense.description} ,date= #{expense.date},category_id= #{expense.categoryId} WHERE expense_id = #{id}
+            RETURNING *
+            """)
+    @ResultMap("expenseMapping")
+    Expense updateExpense(Integer id, @Param("expense") ExpenseRequest expenseRequest, @Param("currentUser") String currentUser);
+
 }
 
 
